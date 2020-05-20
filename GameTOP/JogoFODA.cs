421259d3d0c9eca1745a0be179eb67abafa8882b
Jonas Douglas <jonas.paula@iterative.com.br>
2020-05-20 17:58:10 -0300

    First Commit

---
+++ GameTOP/JogoFODA.cs
@@ -0,0 +1,32 @@
+using System;
+using GameTOP.Interface;
+
+namespace GameTOP
+{
+    public class JogoFODA
+    {
+        private readonly iJogador _jogadorA;
+        private readonly iJogador _jogadorB;
+
+        public JogoFODA(iJogador jogadorA, iJogador jogadorB)
+        {
+            _jogadorA = jogadorA;
+            _jogadorB = jogadorB;
+        }
+
+        public void IniciarJogo()
+        {
+            Console.Write(_jogadorA.Corre());
+            Console.Write(_jogadorA.Chuta());
+            Console.Write(_jogadorA.Passe());
+
+            Console.Write("\n PROXIMO JOGADOR \n");
+
+            Console.Write(_jogadorB.Corre());
+            Console.Write(_jogadorB.Chuta());
+            Console.Write(_jogadorB.Passe());
+
+        }
+
+    }
+}
